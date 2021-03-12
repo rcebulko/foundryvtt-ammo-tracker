93533935efbb6b4c1805922e214984bb5a04bd67
Ryan Cebulko <ryan@cebulko.com>
2021-03-12 12:26:45 -0500

    async-ify to fix race conditions

--- ammo-tracker.js
+++ ammo-tracker.js
@@ -12,15 +12,15 @@ const AmmoTracker = (function () {
     }
 
     /** Records ammo at the start of a combat. */
-    startCombat() {
+    async startCombat() {
       console.log('Ammo Tracker starting combat');
-      this.trackers.forEach(t => t.startCombat());
+      return Promise.all(this.trackers.map(t => t.startCombat()));
     }
 
     /** Records ammo quantities at the end of a combat. */
-    endCombat() {
+    async endCombat() {
       console.log('Ammo Tracker ending combat');
-      this.trackers.forEach(t => t.endCombat());
+      return Promise.all(this.trackers.map(t => t.endCombat()));
     }
   }
 
@@ -41,23 +41,40 @@ const AmmoTracker = (function () {
     }
 
     /** Records ammo at the start of a combat. */
-    startCombat() {
+    async startCombat() {
       this._ammoRecords = {};
-      this.ammoItems.forEach(item => {
-        item.setFlag(FLAG_NAMESPACE, 'startQuantity', item.data.data.quantity);
+      for (const item of this.ammoItems) {
+        // Record initial state
+        await item.setFlag(
+          FLAG_NAMESPACE,
+          'startQuantity',
+          item.data.data.quantity,
+        );
+        await item.setFlag(
+          FLAG_NAMESPACE,
+          'endQuantity',
+          // If the item is used up and removed from inventory, it won't be
+          // updated, so we set this to 0 and assume it'll be overwritten during
+          // `endCombat`
+          0,
+        );
+
         this._ammoRecords[item._id] = item;
-      });
+      }
     }
 
     /** Records ammo quantities at the end of a combat. */
-    endCombat() {
-      this.ammoItems.forEach(item => {
+    async endCombat() {
+      // Set the `endQuantity` flag on all remaining ammo items
+      for (const item of this.ammoItems) {
+        // TODO: This will not handle ammo that is fully spent and removed from
+        // inventory
         if (item._id in this._ammoRecords) {
-          item.setFlag(FLAG_NAMESPACE, 'endQuantity', item.data.data.quantity);
+          await item.setFlag(FLAG_NAMESPACE, 'endQuantity', item.data.data.quantity);
         }
-      });
+      }
 
-      this._notifySpentAmmo();
+      await this.notifySpentAmmo();
     }
 
     /**
@@ -66,36 +83,39 @@ const AmmoTracker = (function () {
      */
     get spentAmmo() {
       return Object.values(this._ammoRecords)
+        // Determine spent/recoverable ammo
         .map(item => {
           const startQuantity = item.getFlag(FLAG_NAMESPACE, 'startQuantity');
           const endQuantity = item.getFlag(FLAG_NAMESPACE, 'endQuantity') || 0;
           const spent = startQuantity - endQuantity;
           const recoverable = Math.floor(spent / 2);
+
           return {item, startQuantity, endQuantity, spent, recoverable};
         })
+        // Ignore unspent ammo
         .filter(({spent}) => spent > 0);
     }
 
     /** Recover half of spent ammo (rounded down). */
-    recoverAmmo() {
+    async recoverAmmo() {
       const recoveredLines = [];
-      this.spentAmmo
-        .forEach(ammo => {
-          const {item, recoverable} = ammo;
-          if (recoverable > 0) {
-            item.update({
-              data: {
-                quantity: item.data.data.quantity + recoverable
-              }
-            });
-            // Mark this off so you can't accidentally double-click
-            ammo.recoverable = 0;
-            recoveredLines.push(`${recoverable}x ${item.name} recovered`);
-          }
-        });
+      // Recover ammo where possible, updating quantity and recording message
+      for (const ammo of this.spentAmmo) {
+        const {item, recoverable} = ammo;
+        if (recoverable > 0) {
+          await item.update({
+            data: {quantity: item.data.data.quantity + recoverable}
+          });
 
+          // Mark this off so you can't accidentally double-click
+          ammo.recoverable = 0;
+          recoveredLines.push(`${recoverable}x ${item.name} recovered`);
+        }
+      };
+
+      // Post a message if anything was recovered
       if (recoveredLines.length) {
-        ChatMessage.create({
+        await ChatMessage.create({
           content: [
             `${this.actor.name} spends a minute recovering ammo`,
             ...recoveredLines,
@@ -106,7 +126,8 @@ const AmmoTracker = (function () {
     }
 
     /** Sends a whisper message about spent and recoverable ammo. */
-    _notifySpentAmmo() {
+    async notifySpentAmmo() {
+      // Build a blurb for each type of ammo spent
       const chatParts = this.spentAmmo.map(
         ({startQuantity, endQuantity, spent, recoverable, item}) => [
           `${item.name}: ${startQuantity} -> ${endQuantity}`,
@@ -115,7 +136,7 @@ const AmmoTracker = (function () {
         ].join('\n'));
 
       if (chatParts.length) {
-        ChatMessage.create({
+        await ChatMessage.create({
           content: [...chatParts, `@Macro[${RECOVER_AMMO_MACRO}]`].join('<hr>'),
           speaker: ChatMessage.getSpeaker({alias: "Ammo Tracker"}),
           type: CHAT_MESSAGE_TYPES.WHISPER, // https://foundryvtt.com/api/foundry.js.html#line83
